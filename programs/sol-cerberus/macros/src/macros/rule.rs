b8e724a0ab48807eff2d20dd39efd09c65719a7b
Ander Ustarroz <anderustarroz@gmail.com>
2023-10-14 16:20:41 +1030

    Added support for dynamic Resources and Permissions

--- programs/sol-cerberus/macros/src/macros/rule.rs
+++ programs/sol-cerberus/macros/src/macros/rule.rs
@@ -39,22 +39,53 @@ pub fn extract_rule(args: &TokenStream) -> (String, String) {
     );
 }
 
-fn find_local_var(block: &syn::Block, var_name: &str) -> Option<usize> {
+fn extract_sc_local_vars(block: &mut syn::Block, var_values: &mut [TokenStream2; 3]) {
     /*
-       Finds the index of a Local var statement. Returns None otherwise.
+       Extracts the following variables from the function body (when present) to apply the dynamically assigned values.
+         - sol_cerberus_app_id
+         - sol_cerberus_resource
+         - sol_cerberus_permission
     */
+    let var_names: [&str; 3] = [
+        "sol_cerberus_app_id",
+        "sol_cerberus_resource",
+        "sol_cerberus_permission",
+    ];
+    let mut found_indexes: Vec<usize> = Vec::new();
     for i in 0..block.stmts.len() {
         if let Stmt::Local(local) = &block.stmts[i] {
+            let mut var_name: Option<&Ident> = None;
+            // Local variable with type defined
             if let Pat::Type(pat_type) = &local.pat {
                 if let Pat::Ident(ident) = &*pat_type.pat {
-                    if ident.ident == var_name {
-                        return Some(i);
+                    var_name = Some(&ident.ident);
+                }
+            // Local variable without type defined
+            } else if let Pat::Ident(ident) = &local.pat {
+                var_name = Some(&ident.ident);
+            }
+            // Find the SC variable and extract the value
+            if var_name.is_some() {
+                for k in 0..3 {
+                    if var_name.unwrap() == var_names[k] {
+                        if let Some(init) = &local.init {
+                            let value = &init.expr;
+                            var_values[k] = quote! {#value};
+                            found_indexes.push(i);
+                        }
+                        break;
                     }
                 }
             }
         }
     }
-    None
+    // Order array in descendent order to avoid breaking block.stmts vector while deleting
+    // the variables that are going to be replaced.
+    found_indexes.sort_by(|a, b| b.cmp(a));
+    // Delete found statements from the function block
+    for index in &found_indexes {
+        block.stmts.remove(*index);
+    }
 }
 
 pub fn rule_macro(args: TokenStream, item: TokenStream) -> TokenStream {
@@ -74,38 +105,31 @@ pub fn rule_macro(args: TokenStream, item: TokenStream) -> TokenStream {
         return proc_macro::TokenStream::from(quote!(#item));
     }
 
-    // Find Sol Cerberus app ID on the current function scope (if defined)
-    let sc_app_id_index = find_local_var(&item.block, "sol_cerberus_app_id");
-    let sc_app_id_var_name: TokenStream2;
-    // Get app ID from local var "sol_cerberus_app_id"
-    if sc_app_id_index.is_some() {
-        // Add it to the beginning of the function block
-        let var_id: Stmt = item.block.stmts.remove(sc_app_id_index.unwrap());
-        item.block.stmts.insert(0, var_id);
-        sc_app_id_var_name = quote! { sol_cerberus_app_id }
-    // Get app ID from global constant "SOL_CERBERUS_APP_ID"
-    } else {
-        sc_app_id_var_name = quote! { SOL_CERBERUS_APP_ID }
-    };
+    // Sol Cerberus var defaults:
+    let mut sc_vars: [TokenStream2; 3] = [
+        quote! { SOL_CERBERUS_APP_ID },
+        quote! { #resource },
+        quote! { #permission },
+    ];
+    // Find Sol Cerberus app ID on the current function scope (when defined)
+    extract_sc_local_vars(&mut item.block, &mut sc_vars);
+    let sc_app_id: &TokenStream2 = &sc_vars[0];
+    let sc_resource: &TokenStream2 = &sc_vars[1];
+    let sc_permission: &TokenStream2 = &sc_vars[2];
     let ctx_arg_name: Ident = ctx_arg_name.unwrap();
     let cpi_call: TokenStream2 = quote! {
         sol_cerberus::cpi::allowed(
             #ctx_arg_name.accounts.sol_cerberus_ctx(),
             sol_cerberus::instructions::AllowedRule {
-                app_id: #sc_app_id_var_name,
+                app_id: #sc_app_id,
                 namespace: 0 as u8, // Rule
-                resource: #resource.to_string(),
-                permission: #permission.to_string(),
+                resource: #sc_resource.to_string(),
+                permission: #sc_permission.to_string(),
             }
         )?;
     };
-    // Add the CPI call either:
-    //  - At the 1st place of the function block
-    //  - At the 2nd place of the function block (when "sol_cerberus_app_id" is defined at 1st place)
-    item.block.stmts.insert(
-        if sc_app_id_index.is_some() { 1 } else { 0 },
-        syn::parse2(cpi_call).unwrap(),
-    );
+    // Add the CPI call either at the beginning of the function block
+    item.block.stmts.insert(0, syn::parse2(cpi_call).unwrap());
     // eprintln!("GENERATED:\r\n{:#?}", quote!(#item).to_string());
     proc_macro::TokenStream::from(quote!(#item))
 }
